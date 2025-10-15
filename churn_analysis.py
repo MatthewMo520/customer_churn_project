23747dd16bd161236a8a5a29588bacde1d48e2af
Matthew Mo <74524092+MatthewMo520@users.noreply.github.com>
2025-10-15 00:51:09 -0400

    cleaned data a bit

--- churn_analysis.py
+++ churn_analysis.py
@@ -1,6 +1,20 @@
 import pandas as pd
 
+# Load the dataset
 data = pd.read_csv('customer_churn.csv')
 
+# Cleaning data 
+
+# removing any numerical data that cannot be converted to numbers
+data["TotalCharges"] = pd.to_numeric(data["TotalCharges"], errors='coerce')
+data = data.dropna()
+
+# converting the data we trying to predict into numerical values
+data["Churn"] = data["Churn"].map({"Yes" : 1, "No" : 0})
+
+# dropping the column because it is not useful
+data = data.drop(["customerID"], axis=1)
+
+
+print(data["Churn"].value_counts())
 print(data.head())
-print(data.info())
