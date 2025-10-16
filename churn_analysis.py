9d3fb7023310d725d831d6093d29ffe57384136b
Matthew Mo <74524092+MatthewMo520@users.noreply.github.com>
2025-10-16 16:56:31 -0400

    divded into training and testing set

--- churn_analysis.py
+++ churn_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # Load the dataset
 data = pd.read_csv('customer_churn.csv')
@@ -18,6 +19,15 @@ data = data.drop(["customerID"], axis=1)
 # one-hot encoding categorical variables
 data = pd.get_dummies(data, drop_first=True)
 
+X = data.drop("Churn", axis=1)
+y = data["Churn"]
 
-print(data.shape)
-print(data.columns)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print("training set:")
+print("X_train shape:", X_train.shape)
+print("y_train shape:", y_train.shape)
+
+print("test set:")
+print("X_test shape:", X_test.shape)
+print("y_test shape:", y_test.shape)
