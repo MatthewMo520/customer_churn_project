25bf064e94674afae7f14ac217abcc4f6a27c8a7
Matthew Mo <74524092+MatthewMo520@users.noreply.github.com>
2025-10-16 17:04:03 -0400

    trained first model

--- churn_analysis.py
+++ churn_analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 # Load the dataset
 data = pd.read_csv('customer_churn.csv')
@@ -19,15 +21,21 @@ data = data.drop(["customerID"], axis=1)
 # one-hot encoding categorical variables
 data = pd.get_dummies(data, drop_first=True)
 
+# splitting into x and y (y is what we predict)
 X = data.drop("Churn", axis=1)
 y = data["Churn"]
 
+# splitting into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-print("training set:")
-print("X_train shape:", X_train.shape)
-print("y_train shape:", y_train.shape)
+# training the model/ creating it
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-print("test set:")
-print("X_test shape:", X_test.shape)
-print("y_test shape:", y_test.shape)
+# making predictions and evaluating the model
+y_pred = model.predict(X_test)
+
+# calculating accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Model Accuracy: {accuracy:.4f}")
+print(f"That's {accuracy * 100:.2f}% accuracy!")
